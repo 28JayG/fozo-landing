783a39892ff8587de0d3be280a61d3405be34440
28JayG <jaygandhi.32@outlook.com>
2025-10-03 23:26:15 +0530

    add hero and initial commit

---
+++ src/components/hero/hero.component.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <section className='font-main font-bold flex flex-col-reverse h-[748px] text-white'>
+      <div className='flex flex-col max-width-container pb-56'>
+        <p className='text-3xl'>Bigger bites,</p>
+        <p className='text-3xl'>
+          -Pocket <span className='font-black-hans text-[44px] text-light-green'>Friendly price!!</span>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
